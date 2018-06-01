50de86ee1f72bd2399b0033abd75718ff797d213
xuzhijie <1050077383@qq.com>
2018-06-01 13:31:11 +0800

    测试推送

--- src/main/java/com/imooc/Imooc.java
+++ src/main/java/com/imooc/Imooc.java
@@ -3,7 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//测试pull
+//测试pull 地方撒旦
 @SpringBootApplication
 public class Imooc {
 
