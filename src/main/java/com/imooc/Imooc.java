929c0d85ef57ff2ca174e75533746eb374b2a608
xuzhijie <1050077383@qq.com>
2018-06-01 11:18:13 +0800

    测试

--- src/main/java/com/imooc/Imooc.java
+++ src/main/java/com/imooc/Imooc.java
@@ -3,7 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//测试git提交--都是
+//测试git提交--都是士大夫
 @SpringBootApplication
 public class Imooc {
 
