6953b981feb12f379744288277d873c2221d43d4
xzjAries <39788898+xzjAries@users.noreply.github.com>
2018-06-01 13:20:03 +0800

    Update Imooc.java

--- src/main/java/com/imooc/Imooc.java
+++ src/main/java/com/imooc/Imooc.java
@@ -3,7 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//测试git提交 大对方的 法大师傅 士大夫官方
+//测试pull
 @SpringBootApplication
 public class Imooc {
 
