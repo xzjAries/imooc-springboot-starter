7874b02c6a644f77edc73d8e6fabbbe00c293fea
xuzhijie <1050077383@qq.com>
2018-06-01 13:39:16 +0800

    测试提交

--- src/main/java/com/imooc/Imooc.java
+++ src/main/java/com/imooc/Imooc.java
@@ -3,7 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//9999-6666
+//9999-6666地方都是 的说法
 @SpringBootApplication
 public class Imooc {
 
