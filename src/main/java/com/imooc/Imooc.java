d47cb7414891b502b97c3bedb0e40a893c04a51a
xuzhijie <1050077383@qq.com>
2018-06-01 13:35:13 +0800

    走起

--- src/main/java/com/imooc/Imooc.java
+++ src/main/java/com/imooc/Imooc.java
@@ -3,7 +3,7 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//测试pull 地方撒旦
+//9999-6666
 @SpringBootApplication
 public class Imooc {
 
